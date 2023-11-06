bc43077ed6f4820cd2e4de4375d05a018bca7135
bervalim <bernardogvalim@gmail.com>
2023-11-06 15:52:07 -0300

    feat/address_entity

--- src/entities/Address.entity.ts
+++ src/entities/Address.entity.ts
@@ -1,2 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
 @Entity("addresses")
-expor
+export default class Adress {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+
+  @Column({ length: 120 })
+  street: string;
+
+  @Column({ length: 8 })
+  ZIP: string;
+
+  @Column({ length: 50 })
+  city: string;
+
+  @Column()
+  number: number;
+
+  @Column({ length: 2 })
+  state: string;
+
+  @Column({ length: 30 })
+  neighborhood: string;
+}
