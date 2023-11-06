6ed167312f92090da326155481c363b2a251faef
bervalim <bernardogvalim@gmail.com>
2023-11-06 18:34:05 -0300

    feat/hash-password

--- src/entities/Client.entity.ts
+++ src/entities/Client.entity.ts
@@ -1,4 +1,6 @@
 import {
+  BeforeInsert,
+  BeforeUpdate,
   Column,
   CreateDateColumn,
   DeleteDateColumn,
@@ -9,6 +11,7 @@ import {
   UpdateDateColumn,
 } from "typeorm";
 import Booking from "./Booking.entity";
+import { getRounds, hashSync } from "bcryptjs";
 
 @Entity("clients")
 export default class Client {
@@ -38,4 +41,11 @@ export default class Client {
 
   @OneToMany(() => Booking, (bookings) => bookings.client)
   bookings: Booking[];
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPassword() {
+    const hasRounds: number = getRounds(this.password);
+    if (!hasRounds) this.password = hashSync(this.password, 10);
+  }
 }
