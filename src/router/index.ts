e621e331d8f8a89e0b11beec37813ba5f68cf2ca
bervalim <bernardogvalim@gmail.com>
2023-11-08 13:22:02 -0300

    feat/session-router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { clientRouter } from "./client.routes";
+import { sessionRouter } from "./session.routes";
 
 export const allRoutes: Router = Router();
 allRoutes.use("/clients", clientRouter);
+allRoutes.use("/login", sessionRouter);
