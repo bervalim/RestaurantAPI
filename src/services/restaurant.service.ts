8a85de2b112719074d95a9bfaab44b87f0879969
bervalim <bernardogvalim@gmail.com>
2023-11-10 14:24:52 -0300

    feat/createRestaurant-Service

--- src/services/restaurant.service.ts
+++ src/services/restaurant.service.ts
@@ -1,9 +1,28 @@
+import Address from "../entities/Address.entity";
 import Category from "../entities/Category.entity";
 import Restaurant from "../entities/Restaurant.entity";
+import AppError from "../errors/App.error";
 import { TcreateRestauranteRequest } from "../interfaces/restaurant.interface";
+import { addressRepo, categoryRepo, restaurantRepo } from "../repositories";
 
 export const createRestaurantService = async (
   requestBody: TcreateRestauranteRequest
 ): Promise<Restaurant> => {
-  const category:Category | null = await 
+  const category: Category | null = await categoryRepo.findOneBy({
+    id: requestBody.categoryId,
+  });
+
+  if (!category) throw new AppError("Category not found!", 404);
+
+  const address: Address = await addressRepo.save(requestBody.address);
+
+  const restaurant: Restaurant = await restaurantRepo.save({
+    ...requestBody,
+    category,
+    address,
+  });
+
+  return restaurant;
 };
+
+export const readAllRestautants = async () => {};
