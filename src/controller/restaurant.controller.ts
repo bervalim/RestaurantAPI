fba2f5e2366681fbfbcb775b1b1e796286bd9ac8
bervalim <bernardogvalim@gmail.com>
2023-11-10 14:48:50 -0300

    feat/readAllRestaurants-Controller

--- src/controller/restaurant.controller.ts
+++ src/controller/restaurant.controller.ts
@@ -1,10 +1,21 @@
 import { Request, Response } from "express";
-import { createRestaurantService } from "../services/restaurant.service";
+import {
+  createRestaurantService,
+  readAllRestaurantsService,
+} from "../services/restaurant.service";
 
 export const createRestaurantController = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const newRestaurant = createRestaurantService(req.body);
+  const newRestaurant = await createRestaurantService(req.body);
   return res.status(201).json(newRestaurant);
+};
+
+export const readAllRestaurantsController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const restaurants = await readAllRestaurantsService();
+  return res.status(200).json(restaurants);
 };
