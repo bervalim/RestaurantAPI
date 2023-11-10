a93b28974e9ee55915a91b570e8fca85f130884e
bervalim <bernardogvalim@gmail.com>
2023-11-10 12:23:52 -0300

    feat/restaurant-router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,10 @@ import { Router } from "express";
 import { clientRouter } from "./client.routes";
 import { sessionRouter } from "./session.routes";
 import { categoryRouter } from "./category.routes";
+import { restaurantRouter } from "./restaurant.routes";
 
 export const allRoutes: Router = Router();
 allRoutes.use("/clients", clientRouter);
 allRoutes.use("/login", sessionRouter);
 allRoutes.use("/categories", categoryRouter);
+allRoutes.use("/restaurant", restaurantRouter);
---
+++ src/router/restaurant.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+export const restaurantRouter: Router = Router();
+restaurantRouter.post("/");
+restaurantRouter.get("/");
