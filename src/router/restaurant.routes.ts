a6016a5aa77df0eeb8472daacc07b073c0239eec
bervalim <bernardogvalim@gmail.com>
2023-11-10 15:15:30 -0300

    feat/verifyRestaurantAddress-Middleware

--- src/router/restaurant.routes.ts
+++ src/router/restaurant.routes.ts
@@ -1,5 +1,21 @@
 import { Router } from "express";
+import {
+  validateBody,
+  verifyAdmin,
+  verifyToken,
+} from "../middlewares/globals.middleware";
+import { createRestaurantRequestSchema } from "../schemas/restaurants.schema";
+import {
+  createRestaurantController,
+  readAllRestaurantsController,
+} from "../controller/restaurant.controller";
 
 export const restaurantRouter: Router = Router();
-restaurantRouter.post("/");
-restaurantRouter.get("/");
+restaurantRouter.post(
+  "/",
+  validateBody(createRestaurantRequestSchema),
+  verifyToken,
+  verifyAdmin,
+  createRestaurantController
+);
+restaurantRouter.get("/", readAllRestaurantsController);
